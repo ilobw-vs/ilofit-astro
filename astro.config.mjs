412434b269975703a8c17463ed3dd592211ab57c
ilobw-vs <ville@ilobw.com>
2026-03-14 15:35:18 +0200

    fdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  output: 'static',
+  compressHTML: true,
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+    }
   },
 });
